7ab9a62b42aadcd462980b62be01492622c8b226
Paxlavon2206 <paxlavon2206@gmail.com>
2024-04-28 10:47:50 +0500

    dars

---
+++ public/profile.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export const Profile = () => {
+  return<svg width={24.000000} height={24.000000} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
+  <desc>
+    Created with Pixso.
+  </desc>
+  <defs>
+    <clipPath id="clip4_18475">
+      <rect id="account_circle" width={24.000000} height={24.000000} fill="white" fillOpacity={0} />
+    </clipPath>
+  </defs>
+  <g clipPath="url(#clip4_18475)">
+    <path id="Vector" d="M12 2C6.47 2 2 6.47 2 12C2 17.52 6.47 22 12 22C17.52 22 22 17.52 22 12C22 6.47 17.52 2 12 2ZM12 5C13.66 5 15 6.34 15 8C15 9.65 13.66 11 12 11C10.34 11 9 9.65 9 8C9 6.34 10.34 5 12 5ZM12 19.2C9.5 19.2 7.28 17.92 6 15.98C6.03 13.99 10 12.9 12 12.9C13.99 12.9 17.96 13.99 18 15.98C16.7 17.92 14.5 19.2 12 19.2Z" fill="#0A0A0A" fillOpacity={1.000000} fillRule="nonzero" />
+  </g>
+</svg>
+
+
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
+import { Profile } from "../../public/profile";
 import { Search } from "../../public/search";
 import { Tune } from "../../public/tune";
+import { Button } from "./components/reasuble-btn/button";
 import { Input } from "./components/reasuble-input/input";
 
 
@@ -7,23 +9,29 @@ import { Input } from "./components/reasuble-input/input";
 export default function Home() {
   return (
     <div>
-      <header className="container pt-6 flex items-center">
+      <header className="pt-6 flex justify-center items-center">
         <div className="mr-[44px]">
-          <img src="/mobilux_logo.png" alt="img" />
+         <a href="/">
+         <img className="w-[116px]" src="/mobilux_logo.png" alt="img" />
+         </a>
         </div>
-        <form className="flex relative items-center mr-5">
+        <form className="flex relative items-center mr-6">
           <div className="absolute pl-4"><Search/></div>
           <Input className="border border-inputB outline-inputB pl-12 py-3 pr-[500px] rounded-l-[6px]" placeholder="Qidirish"/>
           <div className="bg-inputB py-[7px] rounded-r-[6px] px-6 cursor-pointer"><Tune/></div>
         </form>
-       <div className="flex items-center bg-greyBg py-2 px-2 rounded-[2px]">
+       <div className="flex items-center bg-greyBg py-2 px-2 rounded-[2px] mr-5">
        <div className="navbar flex">
-       <select className="text-inputB bg-greyBg"  name="language" id="lang">
+       <select className="text-inputB bg-greyBg outline-none"  name="language" id="lang">
           <option value="Uz">Uz</option>
           <option value="Ru">Ru</option>
           <option value="En">En</option>
         </select>
        </div>
+       </div>
+       <div className="flex">
+       <img className="mr-6" src="/notification.png" alt="img" />
+       <Button className="flex gap-3">Kirish <Profile/> </Button>
        </div>
       </header>
     </div>
